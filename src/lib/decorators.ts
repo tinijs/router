04fd02f8f98513367a40ef46e2b1759ae963e3e0
Lam Nhan <nhan.lh@raksul.com>
2022-12-30 20:05:02 +0700

    Rename .router -> .$router

--- src/lib/decorators.ts
+++ src/lib/decorators.ts
@@ -3,7 +3,7 @@ import {getAppInstance} from '@tinijs/core';
 export function UseRouter() {
   return function (target: Object, propertyKey: string) {
     Reflect.defineProperty(target, propertyKey, {
-      get: () => getAppInstance().router,
+      get: () => getAppInstance().$router,
     });
   };
 }
